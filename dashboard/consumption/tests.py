bb12d96452eab3b1e43225088144b48297d740d6
ghuam <ghuam@hotmail.com>
2017-09-17 09:44:58 +0530

    Completed tests, and the coding challenge.

--- dashboard/consumption/tests.py
+++ dashboard/consumption/tests.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class ConsumptionTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_summary(self):
+        response = self.client.get("/summary/")
+        self.assertEqual(response.status_code, 200)
+
+        #  Response must be rendered with 'layout.html', and 'summary.html'
+        self.assertEqual(len(response.templates), 2)
